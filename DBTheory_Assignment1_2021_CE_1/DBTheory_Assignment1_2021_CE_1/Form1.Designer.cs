d67a795b84cc94aa8246f8f811fc9f4e0e2ba674
Anam Shahzad <111984585+anam309@users.noreply.github.com>
2023-09-24 20:40:59 +0500

    2021_CE_1 Assignment 1
    The uploaded code is a Windows Forms application designed to interact with a database. It provides functionality to insert data into the database, count the total number of records stored within it, and view the records stored in the database.

---
+++ DBTheory_Assignment1_2021_CE_1/DBTheory_Assignment1_2021_CE_1/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace DBTheory_Assignment1_2021_CE_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.textBoxCompany = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonUpdateDb = new System.Windows.Forms.Button();
+            this.textBoxDisplay = new System.Windows.Forms.RichTextBox();
+            this.buttonCountRecords = new System.Windows.Forms.Button();
+            this.buttonViewDb = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(168, 77);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(127, 26);
+            this.textBoxId.TabIndex = 0;
+            // 
+            // textBoxCompany
+            // 
+            this.textBoxCompany.Location = new System.Drawing.Point(168, 129);
+            this.textBoxCompany.Name = "textBoxCompany";
+            this.textBoxCompany.Size = new System.Drawing.Size(127, 26);
+            this.textBoxCompany.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Company Name:";
+            // 
+            // buttonUpdateDb
+            // 
+            this.buttonUpdateDb.Location = new System.Drawing.Point(110, 180);
+            this.buttonUpdateDb.Name = "buttonUpdateDb";
+            this.buttonUpdateDb.Size = new System.Drawing.Size(146, 38);
+            this.buttonUpdateDb.TabIndex = 4;
+            this.buttonUpdateDb.Text = "Update Database";
+            this.buttonUpdateDb.UseVisualStyleBackColor = true;
+            this.buttonUpdateDb.Click += new System.EventHandler(this.buttonUpdateDb_Click);
+            // 
+            // textBoxDisplay
+            // 
+            this.textBoxDisplay.Enabled = false;
+            this.textBoxDisplay.Location = new System.Drawing.Point(383, 77);
+            this.textBoxDisplay.Name = "textBoxDisplay";
+            this.textBoxDisplay.Size = new System.Drawing.Size(364, 78);
+            this.textBoxDisplay.TabIndex = 5;
+            this.textBoxDisplay.Text = "";
+            // 
+            // buttonCountRecords
+            // 
+            this.buttonCountRecords.Location = new System.Drawing.Point(489, 180);
+            this.buttonCountRecords.Name = "buttonCountRecords";
+            this.buttonCountRecords.Size = new System.Drawing.Size(146, 38);
+            this.buttonCountRecords.TabIndex = 6;
+            this.buttonCountRecords.Text = "Count Records";
+            this.buttonCountRecords.UseVisualStyleBackColor = true;
+            this.buttonCountRecords.Click += new System.EventHandler(this.buttonCountRecords_Click);
+            // 
+            // buttonViewDb
+            // 
+            this.buttonViewDb.Location = new System.Drawing.Point(315, 387);
+            this.buttonViewDb.Name = "buttonViewDb";
+            this.buttonViewDb.Size = new System.Drawing.Size(146, 37);
+            this.buttonViewDb.TabIndex = 8;
+            this.buttonViewDb.Text = "View Database";
+            this.buttonViewDb.UseVisualStyleBackColor = true;
+            this.buttonViewDb.Click += new System.EventHandler(this.buttonViewDb_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 246);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(722, 135);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonViewDb);
+            this.Controls.Add(this.buttonCountRecords);
+            this.Controls.Add(this.textBoxDisplay);
+            this.Controls.Add(this.buttonUpdateDb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxCompany);
+            this.Controls.Add(this.textBoxId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.TextBox textBoxCompany;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonUpdateDb;
+        private System.Windows.Forms.RichTextBox textBoxDisplay;
+        private System.Windows.Forms.Button buttonCountRecords;
+        private System.Windows.Forms.Button buttonViewDb;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ DBTheory_Assignment1_2021_CE_1/DBTheory_Assignment1_2021_CE_1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace DBTheory_Assignment1_2021_CE_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Form1_Load() called...");
+            textBoxDisplay.Text = "Startup...";
+            try
+            {
+                System.Diagnostics.Debug.WriteLine("within the try");
+                SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-5UN9QJT\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+                connection.Open();
+                textBoxDisplay.Text = "Connection Successful";
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                textBoxDisplay.Text = "Error, " + ex;
+            }
+        }
+      
+     
+
+        private void buttonCountRecords_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand();
+            SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-5UN9QJT\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+            connection.Open();
+            textBoxDisplay.Text = "Counting Records...";
+            command.Connection = connection;
+            command.CommandText = "select count(*) from Customers";
+            int count = (int)command.ExecuteScalar();
+            textBoxDisplay.Text = "Number of records: " + count;
+            connection.Close();
+        }
+
+        private void buttonUpdateDb_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand();
+            SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-5UN9QJT\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+            connection.Open();
+            textBoxDisplay.Text = "Inserting Record...";
+            command.Connection = connection;
+            command.CommandText = "insert into Customers (CustomerID, CompanyName) values('" +
+            textBoxId.Text + "','" + textBoxCompany.Text + "')";
+            command.ExecuteNonQuery();
+            textBoxDisplay.Text = "Record Inserted...";
+            connection.Close();
+        }
+
+        private void buttonViewDb_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand();
+            SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-5UN9QJT\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+            connection.Open();
+            textBoxDisplay.Text = "Retrieving Records...";
+            command.Connection = connection;
+            command.CommandText = "select * from Customers";
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            textBoxDisplay.Text = "Retrieval Successful!";
+            connection.Close();
+        }
+    }
+}
